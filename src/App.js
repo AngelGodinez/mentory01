97bc883d2bf40171ec02d5a6981ed8b1787f53e8
José Ángel Peña Godínez <jose.pena@wizeline.com>
2022-02-16 09:30:25 -0500

    first commit to master

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [gridSize,setGridSize] = useState(2);
+  const checkValidNumber = (e) => {
+    if ( typeof Number(e.currentTarget.value) === 'number' ) {
+      if (Number(e.currentTarget.value) < 2 ) {
+        setGridSize(2);
+      } else {
+        setGridSize(e.currentTarget.value);
+      }
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter the size of the grid:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="number"
+          step="1"
+          min="2"
+          value={gridSize}
+          onChange={checkValidNumber}/>
+          <button type="button">Start</button>
       </header>
     </div>
   );
